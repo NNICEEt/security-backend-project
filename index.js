4eb476a1b2968cb52e9ea39c3a070d23e6c4cd53
Teerapat Nayapan <74887180+NNICEEt@users.noreply.github.com>
2022-04-17 00:12:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(require("cors")());
+app.use(cors());
 
 app.use(require("./routes"));
 
