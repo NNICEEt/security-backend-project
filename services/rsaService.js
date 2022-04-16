c99d1348cbb91e6425491d46168ff39c87ed02f3
NNICEEt <niceteerapat@gmail.com>
2022-04-17 01:11:27 +0700

    Update rsaService.js

--- services/rsaService.js
+++ services/rsaService.js
@@ -2,33 +2,46 @@ const bigInt = require("big-integer");
 
 const rsaService = {
   async generateKey(keySize) {
-    return new Promise((resolve, reject) => {
-      const p = this.getRandomPrime(keySize / 2);
-      const q = this.getRandomPrime(keySize / 2);
-      const n = p.multiply(q);
-      const phi = p.minus(1).multiply(q.minus(1));
-      let e = this.getRandomPrime(16);
-      while (bigInt.gcd(e, phi).notEquals(1)) e = this.getRandomPrime(16);
-      const d = e.modInv(phi);
-
-      const publicKey = { e, n };
-      const privateKey = { d, n };
-
+    return new Promise(async (resolve, reject) => {
+      let publicKey, privateKey;
+      while (true) {
+        const p = this.getRandomPrime(keySize / 2);
+        const q = this.getRandomPrime(keySize / 2);
+        const n = p.multiply(q);
+        const phi = p.prev().multiply(q.prev());
+        let e = this.getRandomPrime(keySize / 8);
+        while (bigInt.gcd(e, phi).notEquals(1))
+          e = this.getRandomPrim(keySize / 8);
+        const d = e.modInv(phi);
+        publicKey = btoa(JSON.stringify({ e, n }));
+        privateKey = btoa(JSON.stringify({ d, n }));
+        if (await this.verify(publicKey, privateKey)) break;
+      }
       resolve({
         keySize,
         result: {
-          publicKey: btoa(JSON.stringify(publicKey)),
-          privateKey: btoa(JSON.stringify(privateKey)),
+          publicKey,
+          privateKey,
         },
       });
     });
+  },
+  async verify(publicKey, privateKey) {
+    const plainText =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ABC";
+    const encrypted = await this.encrypt(plainText, publicKey);
+    const decrypted = await this.decrypt(
+      encrypted.result.cipherText,
+      privateKey
+    );
+    return plainText === decrypted.result.plainText;
   },
   async encrypt(plainText, key) {
     return new Promise((resolve, reject) => {
       const keyObj = JSON.parse(atob(key));
       const [e, n] = Object.values(keyObj);
       const m = this.encode(plainText);
-      const c = m.modPow(e, n);
+      const c = m.modPow(bigInt(e), bigInt(n));
       const cipherText = btoa(c);
       resolve({ plainText, key, result: { cipherText } });
     });
@@ -38,7 +51,7 @@ const rsaService = {
       const keyObj = JSON.parse(atob(key));
       const [d, n] = Object.values(keyObj);
       const c = atob(cipherText);
-      const m = bigInt(c).modPow(d, n);
+      const m = bigInt(c).modPow(bigInt(d), bigInt(n));
       const plainText = this.decode(m.toString());
       resolve({ cipherText, key, result: { plainText } });
     });
@@ -68,7 +81,7 @@ const rsaService = {
     const max = bigInt.one.shiftLeft(bits).prev();
     while (true) {
       const randNo = bigInt.randBetween(min, max);
-      if (randNo.isProbablePrime(128)) {
+      if (randNo.isProbablePrime(256)) {
         return randNo;
       }
     }
