684d9bfb86b9c38f9589712609869702e4d95eae
NNICEEt <niceteerapat@gmail.com>
2022-04-19 18:36:22 +0700

    add connect timeout

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const timeout = require('connect-timeout')
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
+app.use(timeout('10s'))
 
 app.use(require("./routes"));
 
