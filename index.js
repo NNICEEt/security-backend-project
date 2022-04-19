75251cc20e52ab617e7e22374f60b0977f6202a6
NNICEEt <niceteerapat@gmail.com>
2022-04-19 21:14:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(require("./routes"));
 
